6789644b22d3cbdcc50ba4ac94c3965b39173ad4
Marc-Alexander Grec <marcgrec@tuta.com>
2026-01-09 20:25:33 +0200

    Enable TLP power tuning for x230

--- hosts/thinkpad-x230/default.nix
+++ hosts/thinkpad-x230/default.nix
@@ -39,6 +39,17 @@
     variant = "";
   };
 
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+    };
+  };
+
   users.users.${userConfig.name} = {
     isNormalUser = true;
     description = "${userConfig.fullName}";
