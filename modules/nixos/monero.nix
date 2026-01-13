168e9cb9872eb085f9e8e19a9722ba695662c02f
marcg <marcgrec@tuta.com>
2026-01-13 13:32:28 +0200

    add monero mining

--- modules/nixos/monero.nix
+++ modules/nixos/monero.nix
@@ -1,8 +1,39 @@
 {
+  lib,
+  ...
+}:
+{
   services.monero = {
     enable = true;
     limits.download = -1;
     limits.upload = -1;
     prune = true;
   };
+
+  services.xmrig = {
+    enable = true;
+
+    settings = {
+      autosave = false;
+
+      cpu = {
+        enabled = true;
+      };
+
+      opencl = false;
+      cuda = false;
+
+      pools = [
+        {
+          url = "xmr-eu1.nanopool.org:10343";
+          user = "83mtyXhBWxp7WP9XhtgJyGZDTpt7q62mpCnmtfrfT5ofFbJ6SqSnsey5ndkWxpcTaoEsFPnuJCzXXDVmTuGtNmStDNAKEMf";
+          coin = "monero";
+          keepalive = true;
+          tls = true;
+        }
+      ];
+    };
+  };
+
+  systemd.services.xmrig.wantedBy = lib.mkForce [ ];
 }
