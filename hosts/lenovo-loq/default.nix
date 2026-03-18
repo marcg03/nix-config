57d207aa397b61c797b3e73ca864132a36b6ddf3
marcg <marcgrec@tuta.com>
2026-03-18 17:28:12 +0200

    feat(nvidia-prime): enable offload command

--- hosts/lenovo-loq/default.nix
+++ hosts/lenovo-loq/default.nix
@@ -45,6 +45,10 @@
         finegrained = true;
       };
       prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
