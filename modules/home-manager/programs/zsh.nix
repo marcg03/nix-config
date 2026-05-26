1420648c0c521d595eb799a791c91b3779358de4
marcg <marcgrec@tuta.com>
2026-05-26 10:13:09 +0300

    feat(home-manager): improve zsh config

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -6,11 +6,23 @@
     # autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    defaultKeymap = "viins";
+
+    autocd = true;
+
+    setOptions = [
+      "correct"
+      "extendedglob"
+    ];
+
     history = {
-      size = 5000;
-      save = 5000;
+      size = 10000;
+      save = 10000;
       share = true;
       ignoreAllDups = true;
+      saveNoDups = true;
+      findNoDups = true;
+      ignoreSpace = true;
       ignorePatterns = [
         "rm"
         "rm -f"
@@ -25,13 +37,32 @@
     };
 
     initContent = ''
-      # vi mode
-      bindkey -v
-      export KEYTIMEOUT=1
-
-      setopt autocd
-      setopt correct
-      setopt extendedglob
+      nixify() {
+        if [ ! -e ./.envrc ]; then
+          echo "use nix" > .envrc
+          direnv allow
+        fi
+        if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+          cat > default.nix <<'EOF'
+      with import <nixpkgs> {};
+      mkShell {
+        nativeBuildInputs = [
+          bashInteractive
+        ];
+      }
+      EOF
+          ''${EDITOR:-vim} default.nix
+        fi
+      }
+      flakify() {
+        if [ ! -e flake.nix ]; then
+          nix flake new -t github:nix-community/nix-direnv .
+        elif [ ! -e .envrc ]; then
+          echo "use flake" > .envrc
+          direnv allow
+        fi
+        ''${EDITOR:-vim} flake.nix
+      }
     '';
   };
 }
