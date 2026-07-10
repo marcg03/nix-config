e52d7c9e81d51ff7296282a888f8680872e14fcb
marcg <marcgrec@tuta.com>
2026-07-10 12:04:39 +0300

    add pass extensions

--- modules/home-manager/programs/password-store.nix
+++ modules/home-manager/programs/password-store.nix
@@ -5,6 +5,10 @@
 {
   programs.password-store = {
     enable = true;
-    package = pkgs.pass-wayland.withExtensions (exts: [ exts.pass-otp ]);
+    package = pkgs.pass-wayland.withExtensions (exts: [
+      exts.pass-otp
+      exts.pass-genphrase
+      exts.pass-checkup
+    ]);
   };
 }
