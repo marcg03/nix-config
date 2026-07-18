aac2bfebfdff8f73545f525f00365f9be4c3c6d8
marcg <marcgrec@tuta.com>
2026-07-18 17:21:07 +0300

    update fzf config

--- modules/home-manager/programs/fzf.nix
+++ modules/home-manager/programs/fzf.nix
@@ -1,1 +1,9 @@
-{ programs.fzf.enable = true; }
+{ pkgs, ... }:
+{
+  home.packages = [ pkgs.fd ];
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    fileWidget.zsh.command = "fd --type d";
+  };
+}
