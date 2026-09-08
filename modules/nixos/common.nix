d84dd467b066d233e011998d0e2ae7abbe502bfe
marcg <marcgrec@tuta.com>
2026-09-08 15:05:57 +0300

    feat: set periodic nix GC; use nix store optimization

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -29,8 +29,18 @@
     LC_TIME = "ro_RO.UTF-8";
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    optimise.automatic = true;
+  };
 }
