973b4be43b605f229c27cb8946e91d2455251879
marcg <marcgrec@tuta.com>
2026-09-10 20:49:01 +0300

    feat: enable printing

--- modules/nixos/thinkpad-x230/users/marcg.nix
+++ modules/nixos/thinkpad-x230/users/marcg.nix
@@ -25,6 +25,7 @@ in
     (callUserModule "${self}/modules/nixos/users/programs/tmux.nix")
     (callUserModule "${self}/modules/nixos/users/programs/vesktop.nix")
     (callUserModule "${self}/modules/nixos/users/programs/yazi.nix")
+    (callUserModule "${self}/modules/nixos/users/services/printing.nix")
     (callUserModule "${self}/modules/nixos/users/programs/zsh.nix")
   ];
 
