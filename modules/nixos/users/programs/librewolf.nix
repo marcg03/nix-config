028575fdc70b24a841c9fca7018dfa1f8fd9ed25
marcg <marcgrec@tuta.com>
2026-09-18 09:10:19 +0300

    fix(librewolf): remove hardcoded username

--- modules/nixos/users/programs/librewolf.nix
+++ modules/nixos/users/programs/librewolf.nix
@@ -1,5 +1,5 @@
 user: { self, ... }: {
   home-manager.users.${user}.imports = [ "${self}/modules/home-manager/programs/librewolf.nix" ];
 
-  environment.persistence."/data".users.marcg.directories = [ ".librewolf" ];
+  environment.persistence."/data".users.${user}.directories = [ ".librewolf" ];
 }
